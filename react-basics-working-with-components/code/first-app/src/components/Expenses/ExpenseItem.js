bc2ebf3d5303492677013c17b3b0d29f722a2abf
mdraselmiah95 <russelroy15@gmail.com>
2022-07-11 11:54:42 +0600

    Date formating and added new code

--- react-basics-working-with-components/code/first-app/src/components/Expenses/ExpenseItem.js
+++ react-basics-working-with-components/code/first-app/src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ expenses }) => {
   const { title, amount, date } = expenses;
   return (
     <Card className="expense-item">
-      {/* <ExpenseDate date={props.date} /> */}
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
