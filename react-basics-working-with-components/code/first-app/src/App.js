6944f6de8e65902bc8b67d1eb962f8198a8efefc
mdraselmiah95 <russelroy15@gmail.com>
2022-07-31 18:39:34 +0600

    Learning React Expenses

--- react-basics-working-with-components/code/first-app/src/App.js
+++ react-basics-working-with-components/code/first-app/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -31,6 +32,7 @@ function App() {
   return (
     <div>
       {/* <h2>Let's get started!</h2> */}
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
--- react-basics-working-with-components/code/first-app/src/components/NewExpense/NewExpense.js
+++ react-basics-working-with-components/code/first-app/src/components/NewExpense/NewExpense.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = () => {
   return (
