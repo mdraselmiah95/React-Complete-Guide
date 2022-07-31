3e5d38281044ca77509cc5689a44ab157bd38ebe
mdraselmiah95 <russelroy15@gmail.com>
2022-07-31 22:59:50 +0600

    new code added

--- react-basics-working-with-components/code/first-app/src/components/NewExpense/ExpenseForm.js
+++ react-basics-working-with-components/code/first-app/src/components/NewExpense/ExpenseForm.js
@@ -40,6 +40,15 @@ const ExpenseForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
