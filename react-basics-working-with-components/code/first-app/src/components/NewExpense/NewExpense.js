3986acd7acb35a7cc08b62014b1adda9b98170e7
mdraselmiah95 <russelroy15@gmail.com>
2022-08-01 00:11:03 +0600

    new code added

--- react-basics-working-with-components/code/first-app/src/components/NewExpense/NewExpense.js
+++ react-basics-working-with-components/code/first-app/src/components/NewExpense/NewExpense.js
@@ -3,9 +3,16 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = () => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
