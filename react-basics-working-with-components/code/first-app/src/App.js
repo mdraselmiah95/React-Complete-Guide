4b8582bccdd3fc57ec66bdf9d7976d6f409451f3
mdraselmiah95 <russelroy15@gmail.com>
2022-08-01 00:18:07 +0600

    React props passing

--- react-basics-working-with-components/code/first-app/src/App.js
+++ react-basics-working-with-components/code/first-app/src/App.js
@@ -29,10 +29,15 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   return (
     <div>
       {/* <h2>Let's get started!</h2> */}
-      <NewExpense />
+      <NewExpense onAddExpenses={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
--- react-basics-working-with-components/code/first-app/src/components/NewExpense/NewExpense.js
+++ react-basics-working-with-components/code/first-app/src/components/NewExpense/NewExpense.js
@@ -2,13 +2,13 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+    props.onAddExpenses(expenseData);
   };
   return (
     <div className="new-expense">
