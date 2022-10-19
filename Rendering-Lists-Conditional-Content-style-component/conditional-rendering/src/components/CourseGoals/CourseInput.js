a0fe0b114799fc611bc875ba95df0756387dc841
mdraselmiah95 <russelroy15@gmail.com>
2022-10-19 21:44:47 +0600

    WIP on Conditional Rendering

--- Rendering-Lists-Conditional-Content-style-component/conditional-rendering/src/components/CourseGoals/CourseInput.js
+++ Rendering-Lists-Conditional-Content-style-component/conditional-rendering/src/components/CourseGoals/CourseInput.js
@@ -5,6 +5,7 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
@@ -13,6 +14,7 @@ const CourseInput = (props) => {
   const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
+      setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
@@ -21,7 +23,7 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
