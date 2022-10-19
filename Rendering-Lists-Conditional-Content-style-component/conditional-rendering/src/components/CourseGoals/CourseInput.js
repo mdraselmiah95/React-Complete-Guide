78205a3b9b0c90c623fb9f2138c72969882334e0
mdraselmiah95 <russelroy15@gmail.com>
2022-10-19 21:49:10 +0600

    WIP on Conditional Rendering different style

--- Rendering-Lists-Conditional-Content-style-component/conditional-rendering/src/components/CourseGoals/CourseInput.js
+++ Rendering-Lists-Conditional-Content-style-component/conditional-rendering/src/components/CourseGoals/CourseInput.js
@@ -24,7 +24,14 @@ const CourseInput = (props) => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            backgroundColor: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
