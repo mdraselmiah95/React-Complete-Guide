768df0b7875b1bd170d9fc7f2cf37278a93a92e2
mdraselmiah95 <russelroy15@gmail.com>
2022-10-20 19:00:33 +0600

    WIP on Conditional Rendering

--- Rendering-Lists-Conditional-Content-style-component/conditional-rendering/src/components/CourseGoals/CourseInput.js
+++ Rendering-Lists-Conditional-Content-style-component/conditional-rendering/src/components/CourseGoals/CourseInput.js
@@ -44,6 +44,9 @@ const CourseInput = (props) => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
