408bcf4d4e47ed23ab6e1fbe7bb130b507cb9b22
mdraselmiah95 <russelroy15@gmail.com>
2022-10-20 19:45:38 +0600

    WIP on Practical project one

--- practice-project/practice-project-one/src/components/Users/AddUser.js
+++ practice-project/practice-project-one/src/components/Users/AddUser.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = () => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
