0f6fbfe374a2ac656a25442bbdf624dfbd2a46b4
mdraselmiah95 <russelroy15@gmail.com>
2022-10-20 22:11:56 +0600

    WIP on Practical project one

--- practice-project/practice-project-one/src/App.js
+++ practice-project/practice-project-one/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useId } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
-  const id = useId();
+  const id = Math.random().toString(36).substring(2);
 
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
