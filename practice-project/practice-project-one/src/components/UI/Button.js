85b1eb301c67b3850b9178819ca49cfcf802cc76
mdraselmiah95 <russelroy15@gmail.com>
2022-10-20 19:53:34 +0600

    WIP on Practical project one

--- practice-project/practice-project-one/src/components/UI/Button.js
+++ practice-project/practice-project-one/src/components/UI/Button.js
@@ -2,7 +2,15 @@ import React from "react";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
-  return <div className={classes.button}>{props.children}</div>;
+  return (
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
--- practice-project/practice-project-one/src/components/Users/AddUser.js
+++ practice-project/practice-project-one/src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
@@ -13,7 +14,7 @@ const AddUser = () => {
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
