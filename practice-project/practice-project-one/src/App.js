4b6bbdb9bacb0bcac7bdc642f0cefa764006da10
mdraselmiah95 <russelroy15@gmail.com>
2022-10-20 21:27:14 +0600

    WIP on Practical project one

--- practice-project/practice-project-one/src/App.js
+++ practice-project/practice-project-one/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
