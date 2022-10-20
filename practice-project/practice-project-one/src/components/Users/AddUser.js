c5f0df23746406798cb4324b7b77f96f5d870970
mdraselmiah95 <russelroy15@gmail.com>
2022-10-20 20:01:12 +0600

    WIP on Practical project one

--- practice-project/practice-project-one/src/components/Users/AddUser.js
+++ practice-project/practice-project-one/src/components/Users/AddUser.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = () => {
+  const [enterUserName, setEnterUserName] = useState("");
+  const [enterUserAge, setEnterUserAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
   };
+
+  const userNameChangeHandler = (e) => {
+    setEnterUserName(e.target.value);
+  };
+
+  const userAgeChangeHandler = (e) => {
+    setEnterUserAge(e.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userNameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={userAgeChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
