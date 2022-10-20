cb68fbef6b7ed3cdb06336bc4830f1784e568913
mdraselmiah95 <russelroy15@gmail.com>
2022-10-20 22:08:13 +0600

    WIP on Practical project one

--- practice-project/practice-project-one/src/App.js
+++ practice-project/practice-project-one/src/App.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useId } from "react";
 import "./App.css";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
+  const id = useId();
 
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, { name: uName, age: uAge }];
+      return [...prevUsersList, { name: uName, age: uAge, id: id }];
     });
   };
 
--- practice-project/practice-project-one/src/components/Users/UsersList.js
+++ practice-project/practice-project-one/src/components/Users/UsersList.js
@@ -3,11 +3,12 @@ import Card from "../UI/Card";
 import classes from "./UsersList.module.css";
 
 const UsersList = (props) => {
+  console.log(props.users);
   return (
     <Card className={classes.users}>
       <ul>
         {props.users.map((user) => (
-          <li>
+          <li key={user.id}>
             {user.name} ({user.age} years old)
           </li>
         ))}
