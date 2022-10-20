aeb98169a844eae44860a74018694c0812314dc9
mdraselmiah95 <russelroy15@gmail.com>
2022-10-20 21:16:53 +0600

    WIP on Practical project one

---
+++ practice-project/practice-project-one/src/components/Users/UsersList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./UsersList.module.css";
+
+const UsersList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <h2>j</h2>
+    </Card>
+  );
+};
+
+export default UsersList;
