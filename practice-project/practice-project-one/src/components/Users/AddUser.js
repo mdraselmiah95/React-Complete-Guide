30aad588e4997bb1de1dc22bd9c92467b12af6d8
mdraselmiah95 <russelroy15@gmail.com>
2022-10-21 21:26:18 +0600

    new code added

--- practice-project/practice-project-one/src/components/Users/AddUser.js
+++ practice-project/practice-project-one/src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
@@ -44,7 +45,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +72,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
