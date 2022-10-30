da21b2345ce0c320c1f8b2b9c916fa13455f97b9
mdraselmiah95 <russelroy15@gmail.com>
2022-10-30 21:39:36 +0600

    WIP handling side effect

--- Handling-Side-Effects-Using-Reducers-and-the-Context-API/handling-side-effect/src/components/Login/Login.js
+++ Handling-Side-Effects-Using-Reducers-and-the-Context-API/handling-side-effect/src/components/Login/Login.js
@@ -12,12 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
+    const identifier = setTimeout(() => {
       console.log("This is rendering.");
       setFormIsValid(
         enteredEmail.includes("@") && enteredPassword.trim().length > 6
       );
     }, 500);
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
