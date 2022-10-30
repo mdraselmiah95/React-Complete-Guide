148b27f87927495356cb711ca034e4246c673808
mdraselmiah95 <russelroy15@gmail.com>
2022-10-31 01:09:53 +0600

    WIP on useReducers

--- Handling-Side-Effects-Using-Reducers-and-the-Context-API/handling-side-effect/src/App.js
+++ Handling-Side-Effects-Using-Reducers-and-the-Context-API/handling-side-effect/src/App.js
@@ -1,11 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import MainHeader from "./components/MainHeader/MainHeader";
 import Card from "./components/UI/Card/Card";
 
+// useReducer
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     const storedInfo = localStorage.getItem("isLogged");
@@ -33,7 +49,11 @@ function App() {
       </main>
 
       <Card>
-        <h2>This is cool</h2>
+        <div>
+          Count: {state.count}
+          <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+          <button onClick={() => dispatch({ type: "increment" })}>+</button>
+        </div>
       </Card>
     </React.Fragment>
   );
