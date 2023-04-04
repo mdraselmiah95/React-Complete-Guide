2e6c49ef5136558dac481d853d36d1cbcdaed386
mdraselmiah95 <russelroy15@gmail.com>
2023-04-05 00:52:46 +0600

    react lazy loader and suspense

--- React-MasterClass-Zero-to-Advanced-🔥/new-start/src/App.js
+++ React-MasterClass-Zero-to-Advanced-🔥/new-start/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import AddVideo from "./components/AddVideo";
 import videoDB from "./data/data";
-import { useContext, useReducer, useState } from "react";
+import { useContext, useReducer, useState, lazy, Suspense } from "react";
 import ThemeContext from "./context/ThemeContext";
+import { BallTriangle } from "react-loader-spinner";
+// import Dummy from "./components/Dummy";
+
+const Dummy = lazy(() => import("./components/Dummy.js"));
 
 function App() {
   const [editableVideo, setEditableVideo] = useState(null);
+  const [show, setShow] = useState(false);
 
   const themeContext = useContext(ThemeContext);
 
@@ -26,12 +31,38 @@ function App() {
     }
   }
 
-  const [videos, dispatch] = useReducer(videoReducer, videoDB);
+  // const [videos, dispatch] = useReducer(videoReducer, videoDB);
 
   return (
     <>
       <div className={`App ${themeContext}`}>
-        <AddVideo dispatch={dispatch} editableVideo={editableVideo}></AddVideo>
+        {/* <AddVideo dispatch={dispatch} editableVideo={editableVideo}></AddVideo> */}
+      </div>
+      <div>
+        <button
+          onClick={() => setShow(true)}
+          style={{ padding: "1rem 2rem", color: "black", fontWeight: "bolder" }}
+        >
+          Show
+        </button>
+        {show ? (
+          <Suspense
+            fallback={
+              <BallTriangle
+                height={100}
+                width={100}
+                radius={5}
+                color="#4fa94d"
+                ariaLabel="ball-triangle-loading"
+                wrapperClass={{}}
+                wrapperStyle=""
+                visible={true}
+              />
+            }
+          >
+            <Dummy />
+          </Suspense>
+        ) : null}
       </div>
     </>
   );
