10dc65308d38711c071af4705e99ef2e70d8b6c5
mdraselmiah95 <russelroy15@gmail.com>
2023-06-27 22:12:48 +0600

    WIP on Food App header

--- food-order-app/src/App.jsx
+++ food-order-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <>
-      <h2>Let's get started</h2>
+      <Header />
     </>
   );
 }
---
+++ food-order-app/src/components/Layout/Header.jsx
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImg from "../../assets/meals.jpg";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
