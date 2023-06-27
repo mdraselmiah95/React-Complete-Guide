050bd8ed6946fb58aa308011b14eea6b86e8281c
mdraselmiah95 <russelroy15@gmail.com>
2023-06-28 00:46:09 +0600

    WIP on Food App cart

---
+++ food-order-app/src/components/Meals/AvailableMeals.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  return <div>AvailableMeals</div>;
+};
+
+export default AvailableMeals;
---
+++ food-order-app/src/components/Meals/MealsSummary.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return <div>MealsSummary</div>;
+};
+
+export default MealsSummary;
