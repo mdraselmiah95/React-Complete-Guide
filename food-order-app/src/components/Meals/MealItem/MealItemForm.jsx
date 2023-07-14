e66df3f37118692da4d915d8e53dc5102f08003b
mdraselmiah95 <russelroy15@gmail.com>
2023-07-14 19:28:24 +0600

    WIP on FOOD APP Form

--- food-order-app/src/components/Meals/MealItem/MealItemForm.jsx
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
-const MealItemForm = () => {
-  return <div>MealItemForm</div>;
+const MealItemForm = (props) => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Click");
+  };
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ADD</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
